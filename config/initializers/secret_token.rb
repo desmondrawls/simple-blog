091c855b294477c53d8295186d03bd38fff461b0
Desmond Rawls <captaingrover@gmail.com>
2013-07-11 09:33:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authblog2::Application.config.secret_token = 'f03fc2134dc4fb2bdc400e0ba8224c857fded4ea18d39c0c6bb4aec514fed948ce219608a122e05dd341fcd6e198fb1f396f0c16c3afde1dd276865d6a407342'
